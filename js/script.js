ae033b3d0895eb255d755ee4c566b25a2a5898cd
hudzon-hawk <142737996+hudzon-hawk@users.noreply.github.com>
2023-09-09 22:23:07 +0300

    Video added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const videoPlayer = document.querySelector('.video-player')
+const video = videoPlayer.querySelector('.video')
+const playButton = videoPlayer.querySelector('.play-button')
+
+
+playButton.addEventListener('click', (e) => {
+    if (video.paused) {
+        video.play()
+        e.target.textContent = '\u23F8'
+    } else {
+        video.pause()
+        e.target.textContent = '\u23F5'
+    }
+})
